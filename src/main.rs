2265bd50014b034a804f78201796e629ff623354
SMP <sergio737446@hotmail.it>
2023-03-06 15:50:46 +0100

    Second change

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
     println!("Hello, world!");
     println!("Hello Hans! Beginning changes (feature sergio_test_1)");
+    println!("            While this line is the change in feature/sergio_test_2");
 }
